7c4d29ef34360e4db3147a3dc87cf63c9aeabd53
KXRXM23 <kritvick99@gmail.com>
2026-04-06 15:29:41 +0530

    fix: correct reducer import final

--- Frontend/ecommerce_inventory/src/redux/store/store.js
+++ Frontend/ecommerce_inventory/src/redux/store/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import sidebarReducer from "../reducer/sidebardata";
-import IsLoggedInReducer from "../reducer/IsLoggedInReducer";
+import sidebarReducer from "../reducer/sidebardata.js";
+import IsLoggedInReducer from "../reducer/IsLoggedInReducer.js";
 
 const store = configureStore({
   reducer: {
