4378bf6f3d909aa586c887870b413e9f26417272
KXRXM23 <kritvick99@gmail.com>
2026-04-06 14:54:31 +0530

    fix: correct configureStore casing

--- Frontend/ecommerce_inventory/src/redux/store/store.js
+++ Frontend/ecommerce_inventory/src/redux/store/store.js
@@ -1,17 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import sidebarReducer from "../reducer/sidebardata";
-<<<<<<< HEAD
-import IsLoggedInReducer from "../reducer/IsLoggedInReducer";
-const store=configureStore({
-    reducer:{
-        sidebardata:sidebarReducer,
-        isLoggedInReducer:IsLoggedInReducer
-=======
-const store=configureStore({
-    reducer:{
-        sidebardata:sidebarReducer
->>>>>>> cc9368e6b21427bc48b26647a666ef918d570d3f
-    }
+import sidebarReducer from "../redicer/sidebardata";
+import IsLoggedInReducer from "../redicer/isLoggedInReducer";
+
+const store = configureStore({
+  reducer: {
+    sidebar: sidebarReducer,
+    isLoggedIn: IsLoggedInReducer,
+  },
 });
 
 export default store;
+
